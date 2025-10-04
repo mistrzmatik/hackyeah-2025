63024363791c5e04513d806b0ae2894251a3f95b
Mateusz Kraska <mistrzmatik@gmail.com>
2025-10-04 19:40:03 +0200

    Wygląd zusowy

--- front-src/symulator-emerytalny/src/App.vue
+++ front-src/symulator-emerytalny/src/App.vue
@@ -1,103 +1,111 @@
 <script setup>
-const nextSteps = [
-  { icon: 'settings', title: 'Configuration', text: 'Deeply customize your project with our convenient mechanism.', link: 'https://ui.vuestic.dev/getting-started/configuration-guide'},
-  { icon: 'dashboard', title: 'Kitchensink', text: 'Visit a showcase of all components.', link: 'https://ui.vuestic.dev/getting-started/kitchensink'},
-  { icon: 'group', title: 'Community', text: 'Join our friendly community in Discord.', link: 'https://discord.gg/u7fQdqQt8c'},
-  { icon: 'alt_route', title: 'Tree shaking', text: 'Learn how optimize your bundle size.', link: 'https://ui.vuestic.dev/getting-started/tree-shaking'},
-  { icon: 'handshake', title: 'Contribution', text: 'Become a part of the ongoing development of Vuestic UI.', link: 'https://ui.vuestic.dev/contribution/guide'},
-  { icon: 'star', title: 'Vuestic Admin', text: 'Try our admin template project.', link: 'https://admin.vuestic.dev/'},
-]
+import { ref } from 'vue'
+import { VaButtonToggle, VaButton } from 'vuestic-ui'
+
+const expectedRetirementValue = ref(4000)
+const age = ref(25)
+const gender = ref(0)
+const grossSalary = ref(7500)
+const workFromAge = ref(20)
+const retirementAge = ref(65)
+const savedInZUS = ref(0)
+
+const includeSickLeave = ref(false)
+
+
 </script>
 
 <template>
-  <div class="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="300" height="125" />
-    </header>
 
-    <main>
-      <h5 class="app-title">
-        Congratulations! <br/>You've successfully installed the project.
-      </h5>
+  <VaInput
+      v-model="expectedRetirementValue"
+      placeholder="0.00"
+      type="number"
+      label="Wysokość emerytury w przyszłości"
+    >
+      <template #appendInner>
+        <span>PLN</span>
+      </template>
+    </VaInput>
 
-      <h5 class="app-text">What's next?</h5>
-      <div>
-        <div class="steps-row">
-          <VaCard
-            v-for="step in nextSteps"
-            :href="step.link"
-            target="_blank"
-            class="steps-column"
-          >
-            <VaCardTitle>
-              <VaIcon class="steps-icon" :name="step.icon" />
-              {{ step.title }}
-              <VaIcon name="va-arrow-right" size="small" />
-            </VaCardTitle>
-            <VaCardContent>
-              {{ step.text }}
-            </VaCardContent>
-          </VaCard>
-        </div>
-      </div>
-    </main>
-  </div>
+   <VaInput
+      v-model="age"
+      label="Wiek"
+      type="number"
+      required-mark
+    >
+      <template #appendInner>
+        <span>lat</span>
+      </template>
+    </VaInput>
+
+  <VaButtonToggle
+    v-model="gender"
+    :options="[
+      { label: 'Kobieta', value: 0 },
+      { label: 'Mężczyzna', value: 1 },
+    ]"
+  />
+
+    <VaInput
+      v-model="grossSalary"
+      label="Wysokość wynagrodzenia brutto"
+      placeholder="0.00"
+      type="number"
+      required-mark
+    >
+      <template #appendInner>
+        <span>PLN</span>
+      </template>
+    </VaInput>
+
+    <VaInput
+      v-model="workFromAge"
+      label="Rok rozpoczęcia pracy"
+      type="number"
+      required-mark
+    >
+      <template #appendInner>
+        <span>lat</span>
+      </template>
+    </VaInput>
+
+    <VaInput
+      v-model="retirementAge"
+      label="Planowany rok zakończenia aktywności zawodowej"
+      type="number"
+      required-mark
+    >
+      <template #appendInner>
+        <span>lat</span>
+      </template>
+    </VaInput>
+
+    <VaInput
+      v-model="savedInZUS"
+      label="Wysokość zgromadzonych środków na koncie i na subkoncie w ZUS"
+      placeholder="0.00"
+      type="number"
+    >
+      <template #appendInner>
+        <span>PLN</span>
+      </template>
+    </VaInput>
+
+    <VaCheckbox
+      v-model="includeSickLeave"
+      class="mb-6"
+      label="Uwzględniaj możliwość zwolnień lekarskich"
+    />
+    <VaPopover message="Półroczne zwolnienie lekarskie to około –1,3% składki emerytalnej.">
+      <VaIcon
+        name="info"
+      />
+    </VaPopover>
+
+    <!-- -->
+
+  <VaButton color="primary">Symuluj</VaButton>
 </template>
-
 <style>
-.app {
-  min-height: 100vh;
-  background: linear-gradient(180deg, #2450be, #557de2);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fafafa;
-  padding: 2rem;
-}
-
-.app-title {
-  font-size: 2rem;
-  margin: 1.75rem;
-  text-align: center;
-}
-
-.app-text {
-  margin: 2rem 0;
-  font-size: 1.75rem;
-  text-align: center;
-}
-
-.steps-icon {
-  margin-right: 0.5rem;
-}
-
-.steps-row {
-  margin-top: 1rem;
-  display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  gap: 1.5rem;
-  min-width: 0;
-  max-width: 800px;
-}
-
-@media (max-width: 768px) {
-  .steps-row  {
-    padding: 1rem;
-    grid-template-columns: repeat(1, minmax(0, 1fr));
-  }
-}
-
-.va-card {
-  display: flex;
-  flex-direction: column;
-}
-
-.va-card__content {
-  flex-grow: 1;
-}
-
-.va-card:hover .va-card__title {
-  color: var(--va-primary)
-}
 </style>
--- front-src/symulator-emerytalny/src/main.ts
+++ front-src/symulator-emerytalny/src/main.ts
@@ -5,4 +5,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).use(createVuestic()).mount('#app')
+createApp(App).use(createVuestic({
+      config: {
+        colors: {
+          variables: {
+            // Default colors
+            primary: "#00993f",
+            secondary: "#3f84d2",
+            success: "#00993f",
+            info: "#00416e",
+            danger: "#f05e5e",
+            warning: "#ffc200",
+            gray: "#bec3ce",
+            dark: "#000000",
+          },
+        },
+      },
+    })).mount('#app')
